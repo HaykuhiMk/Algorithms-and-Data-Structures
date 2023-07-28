b9dd523b56342c90f1a1a253a5780c70a1470846
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2023-07-28 21:09:37 +0400

    Create main.cpp

---
+++ MRU-caching-algorithm/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "mru.h"
+
+
+int main() 
+{
+    MRUCache cache(3); 
+    cache.put(1, 10);
+    cache.put(2, 20);
+    cache.put(3, 30);
+    cache.put(4, 40);
+    cache.put(5, 50);
+    cache.display();
+    return 0;
+}

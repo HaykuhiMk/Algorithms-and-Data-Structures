24572442c7df174a8333c4145153d77804161ffe
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2023-07-28 19:54:18 +0400

    Update lru.cpp

--- LRU-caching-algorithm/lru.cpp
+++ LRU-caching-algorithm/lru.cpp
@@ -1,11 +1,3 @@
-// We can use stl container list as a double
-// ended queue to store the cache keys, with
-// the descending time of reference from front
-// to back and a set container to check presence
-// of a key. But to fetch the address of the key
-// in the list using find(), it takes O(N) time.
-// This can be optimized by storing a reference
-//	 (iterator) to each key in a hash map.
 #include "lru.h"
 
 LRUCache::LRUCache(int n) { capacity = n; }

d1db7f558ea08521d7adcf39701e4926a9e4d81d
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2024-12-25 19:35:29 +0400

    Update insertion_sort.py

--- Python/Vector/Algorithms/Sorting-Algorithms/insertion_sort.py
+++ Python/Vector/Algorithms/Sorting-Algorithms/insertion_sort.py
@@ -1,1 +1,13 @@
-#todo
+"""
+This module implements the insertion sort algorithm.
+"""
+
+def insertion_sort(arr: list):
+    """Insertion sort algorithm"""
+    for i in range(len(arr)):
+        tmp = arr[i]
+        j = i - 1
+        while j >= 0 and tmp <= arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = tmp

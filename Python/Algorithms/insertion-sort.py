e59f0718757fde8122ada81b26ba0286c4c4520e
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2025-01-17 11:15:46 +0400

    Create insertion-sort.py

---
+++ Python/Algorithms/insertion-sort.py
@@ -0,0 +1,13 @@
+"""
+This module implements the insertion sort algorithm.
+"""
+
+def insertion_sort(arr: list):
+    """Insertion sort algorithm"""
+    for i in range(1, len(arr)):
+        tmp = arr[i]
+        j = i - 1
+        while j >= 0 and tmp <= arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = tmp

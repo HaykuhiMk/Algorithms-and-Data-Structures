03af34e7e22df124e6c0f64a2008ef2776e4e109
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2025-01-17 12:44:43 +0400

    Create selection_sort.py

---
+++ Python/Algorithms/selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(arr):
+    size = len(arr)
+    for i in range(size - 1):
+        min_index = i
+        for j in range(i + 1, size):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
